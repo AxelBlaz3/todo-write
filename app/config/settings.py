babb8ac0167f39091d1e4c7b9faf073b2b8f3c5e
AxelBlaz3 <karthikgaddam4@gmail.com>
2022-12-02 15:01:09 -0500

    Update mongodb url

--- app/config/settings.py
+++ app/config/settings.py
@@ -1,7 +1,7 @@
 from pydantic import BaseSettings
 
 class Settings(BaseSettings):
-    mongodb_url: str = "mongodb://mongo:27017"
+    mongodb_url: str = "mongodb://localhost:27017"
 
     class Config:
         env_file = '.env'
